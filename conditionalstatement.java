278532a6b147fbeb3c08ecaf4dd7dbe1ea7e1d48
rohan-822048 <mrblack777r@gmail.com>
2025-05-09 09:30:57 +0530

    Add files via upload

---
+++ conditionalstatement.java
@@ -0,0 +1,18 @@
+package Day3;
+
+public class conditionalstatement {
+	
+	public static void main(String[]args)
+	{
+		int age=17;
+		if(age>=18) {
+			System.out.println("you are adult");
+		} else {
+			
+			if (age<=17) {
+				System.out.println("minor");
+			}
+		}
+	}
+
+}
---
+++ variabletask.java
@@ -0,0 +1,32 @@
+package Day2;
+
+public class variabletask {
+	
+	public static void main(String[]args)
+	{
+		byte num=100;
+		System.out.println(num);
+		
+		short s=32000;
+		System.out.println(s);
+		
+		int i=1000000;
+		System.out.println(i);
+		
+		float f=77.822048f;
+		System.out.println(f);
+		
+		double d=31.19122003;
+		System.out.println(d);
+		
+		boolean istrue=true;
+		System.out.println(istrue);
+		
+		char c ='M';
+		System.out.println('M');
+		
+		String S ="rohan";
+		System.out.println("rohan");
+	}
+
+}
